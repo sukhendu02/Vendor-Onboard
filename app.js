4ace43699b9b3fe7081452868c19521f3e422f5b
Sukhendu Mandal <76108642+sukhendu02@users.noreply.github.com>
2024-11-14 15:12:00 +0000

    added some colours

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 // app.js
 
-const express = require('express');
-const path = require('path');
-const hbs = require('hbs')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 const app = express();
 const PORT = 3000;
 
 // Set the view engine to hbs
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 // Set the views directory
-app.set('views', path.join(__dirname, 'views'));
+app.set("views", path.join(__dirname, "views"));
 
 // Register the partials directory
-hbs.registerPartials(path.join(__dirname, 'views', 'partials'));
-
+hbs.registerPartials(path.join(__dirname, "views", "partials"));
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+app.use(express.static(path.join(__dirname, "public")));
 
 // Define a route
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome', message: 'Hello, World!' });
+app.get("/", (req, res) => {
+  res.render("index", { title: "Welcome", message: "Hello, World!" });
 });
 
 // Start the server
